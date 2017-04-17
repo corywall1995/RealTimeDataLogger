e8c7f01151cba7735253ee838aa42f7b1d325c5d
Cory Wall <cwall4@mail.csuchico.edu>
2017-04-17 10:45:41 -0700

    added global array started a function called addAewNumber

---
+++ datalogger.js
@@ -0,0 +1,10 @@
+var numberValues;
+//numberValues = [1,5,9,2];
+
+function addANewNumber() {
+  // Get text input from DOM
+  var toAdd = document.getElementById("dataInput").value;
+  //Check if that variable is a number
+  // IF it is a number then add to the end of the array
+  
+}
