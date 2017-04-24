1ab297dc02280f9f2251951a7ef6e41fc6c1f20e
Cory Wall <cwall4@mail.csuchico.edu>
2017-04-24 10:06:25 -0700

    Created functions to check to make sure the number entered was and number and if so add it to the array and then calulate the average and the sum of the numbers entered into the array and displays them.

--- datalogger.js
+++ datalogger.js
@@ -1,4 +1,4 @@
-var numberValues;
+var numberValues = [];
 //numberValues = [1,5,9,2];
 
 function addANewNumber() {
@@ -6,5 +6,33 @@ function addANewNumber() {
   var toAdd = document.getElementById("dataInput").value;
   //Check if that variable is a number
   // IF it is a number then add to the end of the array
-  
+  if (isNaN(toAdd)) {
+    alert("Input was not a number");
+  } else {
+    numberValues.push(Number(toAdd));
+    document.getElementById('dataInput').value = "";
+    document.getElementById('total').innerHTML = updateTotal();
+    document.getElementById('average').innerHTML = updateAverage();
+  }
+}
+
+function updateTotal() {
+  var runningTotal, i;
+  runningTotal = 0;
+  for (i = 0; i < numberValues.length; i++) {
+    runningTotal += numberValues[i];
+  }
+  return runningTotal;
+}
+
+function updateAverage() {
+  var average, total, length;
+  total = updateTotal();
+  length = numberValues.length;
+  average = total/length;
+  if (total%length == 0) {
+    return average;
+  } else {
+    return average.toFixed(2);
+  }  
 }
